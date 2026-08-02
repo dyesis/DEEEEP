be95e9c49df9eab4144c3eb3ea5222bdfbd72996
dyesis <dyesis.prod@gmail.com>
2026-08-02 16:03:59 +0200

    Add files via upload

---
+++ Scripts/ScriptProcessors/DEEEEP/Interface.js
@@ -0,0 +1,23 @@
+Content.makeFrontInterface(852, 480);
+
+include("ZoomHandler.js");function onNoteOn()
+{
+	
+}
+ function onNoteOff()
+{
+	
+}
+ function onController()
+{
+	
+}
+ function onTimer()
+{
+	
+}
+ function onControl(number, value)
+{
+	
+}
+ 
